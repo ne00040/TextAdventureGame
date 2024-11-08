a9daf9cb982ebd88b01c76636c5d25cf23ec503d
Nmesomachukwu Ekwenibe <ne00040@my.westga.edu>
2024-11-07 23:35:33 -0500

    Added base classes

---
+++ code/Text Adventure Game/src/edu/westga/cs3211/text_adventure_game/model/Location.java
@@ -0,0 +1,5 @@
+package edu.westga.cs3211.text_adventure_game.model;
+
+public class Location {
+
+}
---
+++ code/Text Adventure Game/src/edu/westga/cs3211/text_adventure_game/viewmodel/GameViewModel.java
@@ -0,0 +1,5 @@
+package edu.westga.cs3211.text_adventure_game.viewmodel;
+
+public class GameViewModel {
+
+}
